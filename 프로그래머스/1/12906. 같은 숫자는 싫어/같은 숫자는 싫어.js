14ee0a13361aff9b486f3949f8949465fbb3c774
9JIIIIIN9 <74411978+9JIIIIIN9@users.noreply.github.com>
2024-03-26 22:52:04 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.52 ms, Memory: 93.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,17 @@
+function solution(arr)
+{
+    arr.length = 1000000 ;
+    var answer = [];
+
+    for(i=0;i<arr.length;i++){
+        if (arr[i]!=arr[i+1]){
+            answer.push(arr[i])
+        }
+        else if(arr[i]==arr[i+1]){
+            continue
+        }
+      
+       
+    }
+    return answer;
+}
