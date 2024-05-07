86b5fbf282428958854f3d750756d119986f226f
9JIIIIIN9 <74411978+9JIIIIIN9@users.noreply.github.com>
2024-05-08 05:17:38 +0900

    [level 2] Title: 숫자의 표현, Time: 0.23 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var count = 0;
+    for(let i=1;i<=n;i++){
+        if(n%i==0 && i%2==1){
+            count++;
+            continue
+        }
+    }
+   
+    return count;
+}
