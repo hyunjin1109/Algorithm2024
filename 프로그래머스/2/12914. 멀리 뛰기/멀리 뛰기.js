38689d94b24ddb8da25b1c7a4f8848473aec44bc
9JIIIIIN9 <74411978+9JIIIIIN9@users.noreply.github.com>
2024-05-13 23:15:39 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    if (n === 1 || n === 2) return n;
+    
+    const dp = Array(n).fill(0)
+    dp[0] = 1;
+    dp[1] = 2;
+    for(let i=2;i<n;i++){
+        dp[i]=(dp[i-1]+dp[i-2])%1234567;
+    }
+    return dp[n-1];
+}
+
