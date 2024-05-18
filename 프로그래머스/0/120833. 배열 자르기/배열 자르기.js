eb25a12d71e934c43d8d4b008fe8c0785b85c29d
9JIIIIIN9 <74411978+9JIIIIIN9@users.noreply.github.com>
2024-05-18 21:47:53 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -1,8 +1,11 @@
 function solution(numbers, num1, num2) {
-    var answer = [];
-    numbers[num1]-numbers[num2]
-    for(let i=num1;i<=num2;i++){
-         answer.push(numbers[i])
-    }
+    // 둘다 가능
+    // var answer = [];
+    // numbers[num1]-numbers[num2]
+    // for(let i=num1;i<=num2;i++){
+    //      answer.push(numbers[i])
+    // }
+    // return answer;
+    var answer= numbers.slice(num1,num2+1);
     return answer;
 }
