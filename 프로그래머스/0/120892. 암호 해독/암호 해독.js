83938e02491f7ce6f90991dce3b5295a7fd514bf
9JIIIIIN9 <74411978+9JIIIIIN9@users.noreply.github.com>
2024-05-20 22:13:07 +0900

    [level 0] Title: 암호 해독, Time: 6.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,13 +1,22 @@
+// function solution(cipher, code) {
+//     var answer = '';
+//     //cipher[3] cipher[7] cipher[11] --- cipher[23]
+//     //4n-1 여기서 24
+//     var space = parseInt(cipher.length/code)
+//     for(let i=1;i<=space;i++){
+//          answer+=(cipher[code*i-1])
+//     } 
+    
+    
+//     return answer;
+// }
+
 function solution(cipher, code) {
-    var answer = '';
-    //cipher[3] cipher[7] cipher[11] --- cipher[23]
-    //4n-1 여기서 24
-    var space = parseInt(cipher.length/code)
-    console.log(space);
-    for(let i=1;i<=space;i++){
-         answer+=(cipher[code*i-1])
-    } // cipher/code
-    
-    
-    return answer;
+    let a= cipher.split('');
+    let b =[]
+    console.log(a);
+    for(i=1; i<=a.length; i++){
+        b.push(a[i*code-1])
+    }
+    return b.join('');
 }
