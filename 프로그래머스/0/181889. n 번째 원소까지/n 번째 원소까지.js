58457629b40bc569b3f11b594e3122213bfece21
9JIIIIIN9 <74411978+9JIIIIIN9@users.noreply.github.com>
2024-08-25 03:02:56 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -0,0 +1,8 @@
+function solution(num_list, n) {
+    var answer = [];
+    num_list[0], num_list[1], num_list[2]
+    for(let i = 0; i<n; i++){
+        answer[i] = num_list[i];
+    }
+    return answer;
+}
