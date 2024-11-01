8803dd4d5ed420cbaf787d59be596fe9f01fd3ae
이현진 <74411978+hyunjin1109@users.noreply.github.com>
2024-11-01 22:07:43 +0900

    [Bronze IV] Title: 베라의 패션, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "dev/stdin" : "run/input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+let a = input[0] * (input[0] - 1);
+console.log(a);
